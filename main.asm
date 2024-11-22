deebd57302a2f29446d356f404836af079516ab5
Facundo Pareja <56973330+facundopareja@users.noreply.github.com>
2024-11-21 22:44:58 -0300

    feat: mostly working usart module

--- main.asm
+++ main.asm
@@ -1,17 +1,107 @@
-.include "usart.asm"
 ;
 ; TP Integrador.asm
 ;
 ; Created: 15/11/2024 08:18:40
 ; Author : Facundo
 ;
-CONFIGURATION:
-; Configuracion inicial
-	rcall USART_Init
-	ldi r16, 0
+.equ caracter_config_mode = 'C'
+.equ caracter_config_finished = 'F'
+; Possible states
+.equ CONFIG_STATE = 1
+.equ LOCK_STATE = 2
+.equ RECEIVED_CODE_STATE = 3
+; Constants
+.equ LENGTH_CODE = 4
+; Register labels
+.DEF temp= r16
+.DEF mode= r17
+.DEF value_received = r18
+.DEF numbers_received = r19
+; RAM
+.dseg
+.ORG SRAM_START
+KEYCODE: .byte 4
 
-; Replace with your application code
+.cseg
+.ORG 0x0000
+	rjmp RESET
+
+.ORG URXCaddr
+	rjmp USART_Receive
+
+.ORG UTXCaddr
+	rjmp USART_Transmit ; La funcion esta incompleta
+
+.ORG INT_VECTORS_SIZE
+RESET:
+	CLI
+	LDI temp, HIGH(RAMEND)					
+	OUT SPH, temp				
+	LDI temp, LOW(RAMEND)
+	OUT SPL, temp
+
+PORT_INITIALIZING:
+	in temp, ddrb
+	ori temp, 0b00110000 ; Mascara para activar PB4/PB5 como salida
+	out ddrb, temp
+
+MODULE_INITIALIZING:
+	rcall USART_Init
+	sei
+
+LOADING_CURRENT_PASSWORD:
+	ldi numbers_received, LENGTH_CODE
+	clr eeprom_address_high
+	clr eeprom_address_low
+TRANSMIT_LOOP:
+	lds r17, UCSR0A
+	sbrs r17, UDRE0
+	rjmp TRANSMIT_LOOP
+	rcall EEPROM_READ
+	mov temp, eeprom_dato
+	rcall USART_Transmit
+	inc eeprom_address_low
+	dec numbers_received
+	cpi numbers_received, 0
+	brne TRANSMIT_LOOP
+
 start:
-    rcall USART_Transmit
-	inc r16
-    rjmp start
+	cpi mode, CONFIG_STATE
+	brne start
+	rcall CONFIG_MODE
+	rjmp start
+
+CONFIG_MODE:
+	clr numbers_received
+	in temp, pinb
+	ori temp, 0b00110000 
+	out portb, temp ; Prendo LEDs
+WAIT_CHAR:
+	cpi mode, RECEIVED_CODE_STATE
+	breq STORE_CODE
+	cpi mode, LOCK_STATE
+	breq END_CONFIG_MODE
+	rjmp WAIT_CHAR
+	clr eeprom_address_high
+	clr eeprom_address_low
+	ldi XH, high(KEYCODE)
+	ldi XL, low(KEYCODE)
+	; Habria que guardar en RTC en caso de que se haya ingresado T
+STORE_CODE:
+	ldi numbers_received, LENGTH_CODE
+LOOP_STORE:
+	ld eeprom_dato, X+
+	rcall EEPROM_WRITE
+	inc eeprom_address_low
+	dec numbers_received
+	cpi numbers_received, 0 
+	brne LOOP_STORE
+END_CONFIG_MODE:
+	in temp, pinb
+	andi temp, 0b11001111 
+	out portb, temp ; Apago leds
+	ret
+
+.include "usart.asm"
+.include "eeprom.asm"
+
--- usart.asm
+++ usart.asm
@@ -5,10 +5,9 @@
  *   Author: Facundo
  */ 
 
-.equ valor_UBRRn=0
+.equ valor_UBRRn=103
 .equ USART_mode = (0<<UMSEL01) | (0<<UMSEL00)
-
-rjmp CONFIGURATION
+.equ UCSR0B_values =  (1<<RXCIE0) | (0<<TXCIE0) | (1<<RXEN0) | (1<<TXEN0) ; Activo transmisor, receptor, interrupciones de transmision y recepcion.
 
 USART_Init:
 	ldi r17, high(valor_UBRRn)
@@ -16,20 +15,48 @@ USART_Init:
 	; Set baud rate to UBRR0
 	sts UBRR0H, r17
 	sts UBRR0L, r16
-	; Enable transmitter, disable receiver.
-	ldi r16, (0<<RXEN0)|(1<<TXEN0)
-	sts UCSR0B,r16
+	ldi r16, UCSR0B_VALUES
+	sts UCSR0B, r16
 	; Set frame format: 8data, 1stop bit 
 	; Setting USART in async mode
 	ldi r16, (3<<UCSZ00)|(0<<USBS0) | USART_mode
-	sts UCSR0C,r16
+	sts UCSR0C, r16
 	ret
 
 USART_Transmit:
-	; Wait for empty transmit buffer
-	lds r17, UCSR0A
-	sbrs r17, UDRE0
-	rjmp USART_Transmit
-	; Put data (r16) into buffer, sends the data
-	sts UDR0,r16
+	sts UDR0, temp
 	ret
+	
+USART_Receive:
+	lds value_received, UDR0
+	mov temp, value_received
+	rcall USART_Transmit
+	cpi mode, CONFIG_STATE
+	breq VERIFY_NUMBER
+	cpi value_received, caracter_config_mode
+	breq SET_CONFIG_MODE
+	cpi value_received, caracter_config_finished
+	; Faltaria validar que reciba una T
+	brne FIN
+	ldi mode, LOCK_STATE
+	rjmp FIN
+SET_CONFIG_MODE:
+	ldi mode, CONFIG_STATE
+	rjmp FIN
+VERIFY_NUMBER:
+	cpi value_received, '0'
+	brlo FIN
+	cpi value_received, '9'+1
+	brsh FIN
+	ldi XH, high(KEYCODE)
+	ldi XL, low(KEYCODE)
+	add XL, numbers_received
+	clr temp
+	adc XH, temp
+	st X+, value_received  
+	inc numbers_received
+	cpi numbers_received, LENGTH_CODE
+	brne FIN
+	ldi mode, RECEIVED_CODE_STATE
+FIN:
+	reti
